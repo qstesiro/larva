a310c0a450ef1922c00bd5d8622c03c2cfc6252c
U-INTERNAL\huangmengmeng01 <huangmengmeng01@6C148940D795492.internal.baidu.com>
2016-06-24 16:32:03 +0800

    abc

--- Debugger/src/com/runbox/debug/command/variant/VariantStringCommand.java
+++ Debugger/src/com/runbox/debug/command/variant/VariantStringCommand.java
@@ -1,12 +1,12 @@
 package com.runbox.debug.command.variant;
 
 import com.runbox.debug.parser.expression.Expression;
+import com.runbox.debug.parser.expression.token.operand.FieldOperand;
 import com.runbox.debug.parser.expression.token.operand.Operand;
 import com.sun.jdi.ArrayReference;
 import com.sun.jdi.ClassType;
 import com.sun.jdi.Field;
 import com.sun.jdi.ObjectReference;
-import com.sun.tools.javac.util.ArrayUtils;
 
 /**
  * Created by huangmengmeng01 on 2016/6/23.
@@ -35,34 +35,32 @@ public class VariantStringCommand extends VariantCommand {
         } else {
             Operand operand = new Expression(argument.trim()).handle();
             if (string(operand)) {
-                print(array(operand));
+                print(array(operand), 0);
             }
         }
         return super.execute();
     }
 
-    private void print(ArrayReference array) throws Exception {
-        if (null != array) {
+    private void print(Operand operand, int count) throws Exception {
+        if (null != operand && 0 < count) {
+            ArrayReference array = (ArrayReference)operand.value();
             for (int i = 0; i < array.length(); ++i) {
                 System.out.print(array.getValue(i));
-            }
-            System.out.println();
-        }
-    }
-
-    private void print(ArrayReference array, int count) throws Exception {
-        if (null != array) {
-            for (int i = 0; i < array.length(); ++i) {
-                System.out.print(array.getValue(i));
-                if (0 == (i + 1) % count && 0 != i) {
+                if (0 == (i + 1) % count && 0 != i && array.length() != (i + 1)) {
                     System.out.println();
                 }
             }
             System.out.println();
+        } else if (0 == count) {
+            ArrayReference array = (ArrayReference)operand.value();
+            for (int i = 0; i < array.length(); ++i) {
+                System.out.print(array.getValue(i));
+            }
+            System.out.println();
         }
     }
 
-    private ArrayReference array(Operand operand) throws Exception {
+    private FieldOperand array(Operand operand) throws Exception {
         if (standard(operand)) {
             return field(operand, "value");
         } else if (builder(operand)) {
@@ -116,13 +114,11 @@ public class VariantStringCommand extends VariantCommand {
         return false;
     }
 
-    protected ArrayReference field(Operand operand, String name) throws Exception {
+    protected FieldOperand field(Operand operand, String name) throws Exception {
         if (null != operand) {
             ObjectReference object = (ObjectReference)operand.value();
             Field field = object.referenceType().fieldByName(name);
-            if (null != field) {
-                return (ArrayReference)object.getValue(field);
-            }
+            return new FieldOperand(object, field.name());
         }
         return null;
     }
