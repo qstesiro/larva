54d77277d493d23b1070c21741473a74032f6480
huangmengmeng01 <huangmengmeng01@baidu.com>
2017-06-08 17:55:02 +0800

    thread.stack support line

--- src/main/java/com/runbox/debug/command/thread/ThreadStackCommand.java
+++ src/main/java/com/runbox/debug/command/thread/ThreadStackCommand.java
@@ -3,13 +3,20 @@ package com.runbox.debug.command.thread;
 import com.sun.jdi.*;
 
 import com.runbox.debug.manager.ContextManager;
+import com.runbox.debug.script.expression.Expression;
 
 public class ThreadStackCommand extends ThreadCommand {
 
     public ThreadStackCommand(String command) throws Exception {
         super(command);
+		if (null != argument()) {
+			flags = new Expression(argument()).execute().getInteger(0);
+		}
     }
 
+	
+	private int flags = FLAG_PACKAGE | FLAG_LINE;
+	
     @Override
     public boolean execute() throws Exception {
         ThreadReference thread = ContextManager.instance().current();
@@ -17,11 +24,30 @@ public class ThreadStackCommand extends ThreadCommand {
             System.out.printf("%-5s%s\n", "#", "location");
 			if (0 < thread.frameCount()) {
 				int i = 0; for (StackFrame frame : thread.frames()) {
-					Location location = frame.location();
-					System.out.printf("%-5d%s.%s\n", i++, location.declaringType().name(), location.method().name());
+					print(i++, frame.location());					
 				}
 			}
         }
         return super.execute();
-    }    
+    }
+
+	private static final int FLAG_PACKAGE = 0x1;	
+	private static final int FLAG_LINE = 0x2;
+	private static final int FLAG_MAX = 0x2;
+	
+	private void print(int index, Location location) {
+		System.out.printf("%-5d", index);
+		if (FLAG_PACKAGE == (FLAG_PACKAGE & flags)) {
+			System.out.printf("%s.", location.declaringType().name());
+		}
+		System.out.printf("%s", location.method().name());
+		if (FLAG_LINE == (FLAG_LINE & flags)) {
+			if (-1 != location.lineNumber()) {
+				System.out.printf(":%d", location.lineNumber());
+			} else  {
+				System.out.printf(":%s", "n/a");
+			}
+		}
+		System.out.println();
+	}	
 }
