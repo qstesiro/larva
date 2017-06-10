acb9c9daedf22f191664646ff4ab6afc1eab7d83
qstesiro <runbox@163.com>
2017-06-10 15:23:42 +0800

    修改config.query命令，可以一次查询多个配制选项

--- src/main/java/com/runbox/command/config/ConfigQueryCommand.java
+++ src/main/java/com/runbox/command/config/ConfigQueryCommand.java
@@ -1,5 +1,8 @@
 package com.runbox.command.config;
 
+import java.util.List;
+import java.util.LinkedList;
+
 import com.runbox.manager.ConfigManager;
 
 import com.runbox.script.expression.Expression;
@@ -11,33 +14,53 @@ public class ConfigQueryCommand extends ConfigCommand {
 	public ConfigQueryCommand(String command) throws Exception {
 		super(command);
 		if (null != argument()) {
-			name = ExpressionFactory.build(argument()).execute().getString(0);			
-		}		
+			values = ExpressionFactory.build(argument()).execute();
+		}
 	}
 
-	private String name = null;
+	private Expression.Values<? extends Token> values = null;
 	
 	@Override
 	public boolean execute() throws Exception {
-		if (null != name) print(name); else print();		
+		System.out.printf("%-5s%-10s%s\n", "#", "name", "value");
+		List<String> names = names();
+		if (0 < names.size()) print(names); else print();		
 		return super.execute();
 	}
 
-	private void print(String name) throws Exception {
+	private List<String> names() throws Exception {
+		List<String> names = new LinkedList<String>();
+		if (null != values) {
+			for (int i = 0; i < values.size(); ++i) {
+				names.add(values.getString(i));
+			}
+		}
+		return names;
+	}
+
+	private void print(List<String> names) throws Exception {
+		if (null != names) {
+			int i = 0; for (String name : names) {
+				print(i, name);
+			}
+		}
+	}
+	
+	private void print(int i, String name) throws Exception {
 		if (null != name) {
 			String value = null;
 			if (name.equals(ConfigManager.IP)) {
-				System.out.println(name + ": " + ConfigManager.instance().ip());
+				System.out.printf("%-5d%-10s%s\n", i++, ConfigManager.IP, ConfigManager.instance().ip());
 			} else if (name.equals(ConfigManager.PORT)) {
-				System.out.println(name + ": " + ConfigManager.instance().port());
+				System.out.printf("%-5d%-10s%s\n", i++, ConfigManager.PORT, ConfigManager.instance().port());
 			} else if (name.equals(ConfigManager.SCRIPT)) {
-				System.out.println(name + ": " + ConfigManager.instance().script());
+				System.out.printf("%-5d%-10s%s\n", i++, ConfigManager.SCRIPT, ConfigManager.instance().script());
 			} else if (name.equals(ConfigManager.MODE)) {
-				System.out.println(name + ": " + ConfigManager.instance().mode());
+				System.out.printf("%-5d%-10s%s\n", i++, ConfigManager.MODE, ConfigManager.instance().mode());
 			} else if (name.equals(ConfigManager.LINE)) {
-				System.out.println(name + ": " + ConfigManager.instance().line());
+				System.out.printf("%-5d%-10s%d\n", i++, ConfigManager.LINE, ConfigManager.instance().line());
 			} else if (name.equals(ConfigManager.BYTECODE)) {
-				System.out.println(name + ": " + ConfigManager.instance().bytecode());
+				System.out.printf("%-5d%-10s%b\n", i++, ConfigManager.BYTECODE, ConfigManager.instance().bytecode());
 			} else {
 				throw new Exception("invalid config name");
 			}
@@ -45,7 +68,7 @@ public class ConfigQueryCommand extends ConfigCommand {
 	}
 
 	private void print() {
-		int i = 0; System.out.printf("%-5s%-10s%s\n", "#", "name", "value");
+		int i = 0;
 		System.out.printf("%-5d%-10s%s\n", i++, ConfigManager.IP, ConfigManager.instance().ip());
 		System.out.printf("%-5d%-10s%s\n", i++, ConfigManager.PORT, ConfigManager.instance().port());
 		System.out.printf("%-5d%-10s%s\n", i++, ConfigManager.SCRIPT, ConfigManager.instance().script());
