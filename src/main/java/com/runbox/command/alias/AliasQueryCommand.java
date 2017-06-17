4ba658d55dc2b58708e4ad68000a6fa046b15e46
qstesiro <runbox@163.com>
2017-06-17 17:54:01 +0800

    change colume width

--- src/main/java/com/runbox/command/alias/AliasQueryCommand.java
+++ src/main/java/com/runbox/command/alias/AliasQueryCommand.java
@@ -12,10 +12,10 @@ public class AliasQueryCommand extends AliasCommand {
 
     @Override
     public boolean execute() throws Exception {
-        System.out.printf("%-5s%-24s%s\n", "#", "command", "alias");
+        System.out.printf("%-5s%-32s%s\n", "#", "command", "alias");
 		Map<String, String> alias = AliasManager.instance().get();		
 		int i = 0; for (String key : alias.keySet()) {			
-			System.out.printf("%-5d%-24s%s\n", i++, alias.get(key), key);
+			System.out.printf("%-5d%-32s%s\n", i++, alias.get(key), key);
 		}
         return super.execute();
     }
